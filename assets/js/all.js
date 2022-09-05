babd071011e1705f17684fc899e5d1baf2b3aabb
ChenHelena <cocolulu2327@gmail.com>
2022-09-05 10:09:07 +0800

    Update 2022-09-05T02:09:04.139Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,18 +1,19 @@
+"use strict";
+
 //navbar
 $(function () {
   $(".navbar-toggler").on("click", function () {
-    $(this).find(".fa-solid").toggleClass("d-none")
+    $(this).find(".fa-solid").toggleClass("d-none");
   });
-})
-
+});
 $(function () {
   $(".searchBtn").on("click", function () {
-    $(".navbar-show").toggleClass("d-none")
+    $(".navbar-show").toggleClass("d-none");
   });
-})
-
+});
 $(function () {
   $(".navbar-return").on("click", function () {
-    $(".navbar-show").toggleClass("d-none")
+    $(".navbar-show").toggleClass("d-none");
   });
-})
+});
+//# sourceMappingURL=all.js.map
