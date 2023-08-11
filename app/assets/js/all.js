ff2cae7dfaaa9e0b6070567ddd209ae0d65f7c16
ChenHelena <cocolulu2327@gmail.com>
2023-08-11 12:08:54 +0800

    'update'

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -16,3 +16,38 @@ $(function () {
     $(".navbar-show").toggleClass("d-none")
   });
 })
+
+const swiper = new Swiper('.swiper-container', {
+    centeredSlides: true,
+    loop: true,
+    speed: 500,
+    spaceBetween: 0,
+    slidesPerView: 1,
+    // autoplay: {
+    //     delay: 3000,
+    // },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+     breakpoints: {
+      1200: {
+        centeredSlides: true,
+        slidesPerView: 3,
+        spaceBetween: 24,
+      }
+    },
+    pagination: {
+          el: ".swiper-pagination",
+
+          clickable: true,
+        },
+    
+});
+
+//masonry
+$('.grid').masonry({
+  itemSelector: '.grid-item', // use a separate class for itemSelector, other than .col-
+  columnWidth: '.grid-sizer',
+  percentPosition: true
+});
